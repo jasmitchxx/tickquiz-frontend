45f7f5d3d2be3b6fc53f76a8d18daa651022b311
Brakwasi <Jasmitch2025@gmail.com>
2025-06-04 02:17:45 +0000

    Fix: Add missing /start route to App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import VerifyPaymentPage from './pages/VerifyPaymentPage';
 import PaymentSuccessPage from './pages/PaymentSuccessPage';
 import QuizPage from './pages/QuizPage';
 import PaymentFailedPage from './pages/PaymentFailedPage';
-import UseAccessCodePage from './pages/UseAccessCodePage'; // Optional
+import UseAccessCodePage from './pages/UseAccessCodePage';
+import QuizStartPage from './pages/QuizStartPage'; // ? Add this line
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RequestAccessWithPayment />} />
+        <Route path="/start" element={<QuizStartPage />} /> {/* ? Add this route */}
         <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/use-code" element={<UseAccessCodePage />} /> {/* Optional */}
-<Route path="/verify" element={<VerifyPaymentPage />} />
+        <Route path="/use-code" element={<UseAccessCodePage />} />
+        <Route path="/verify" element={<VerifyPaymentPage />} />
         <Route path="/payment-success" element={<PaymentSuccessPage />} />
         <Route path="/payment-failed" element={<PaymentFailedPage />} />
       </Routes>
